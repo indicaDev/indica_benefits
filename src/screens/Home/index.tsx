b9db028641c11e83f269cd644ee834788b9b9ccc
Gilson Soares Sales <indicacursos14@gmail.com>
2023-03-05 13:13:25 -0300

    feat: implementação do valor disponível e do botão de visualizar o extrato

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,5 @@
-import { View } from "react-native";
+import { Alert, Text, TouchableOpacity, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 import { Header } from "./components/Header";
 
@@ -8,6 +9,17 @@ export function Home() {
   return (
     <View style={styles.container}>
       <Header />
+
+      <Text style={styles.balanceAvailable}>
+        Você possui R$ 700,50 disponível no seu cartão.
+      </Text>
+      <TouchableOpacity
+        style={styles.seeExtractButton}
+        onPress={() => Alert.alert("Ver o estrato")}
+      >
+        <Text style={styles.seeExtractTitle}>Ver meu extrato</Text>
+        <Ionicons name="ios-arrow-forward" size={30} color="#5D5FEF" />
+      </TouchableOpacity>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -6,6 +6,25 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFFFFF",
     paddingHorizontal: 20,
   },
+  balanceAvailable: {
+    fontSize: 24,
+    fontWeight: "500",
+    lineHeight: 36,
+    color: "#9796A1",
+    marginTop: 33,
+  },
+  seeExtractButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    alignSelf: "center",
+    marginTop: 90,
+  },
+  seeExtractTitle: {
+    fontSize: 20,
+    fontWeight: "500",
+    color: "#5B5B5E",
+    marginRight: 5,
+  },
 });
 
 export default styles;
